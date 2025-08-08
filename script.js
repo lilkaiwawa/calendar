7a594713df7d39d402e4c2b498655390f3a04cf3
lilkaiwawa <shopping_expert@hotmail.com>
2025-08-09 00:49:11 +1200

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const today = new Date();
 
+// Get the weekday name and day number
 const dayNumber = today.getDate();
 const weekday = today.toLocaleString('default', { weekday: 'long' });
 
+// Set the content in the DOM
 document.getElementById("day").textContent = dayNumber;
 document.getElementById("weekday").textContent = weekday;
